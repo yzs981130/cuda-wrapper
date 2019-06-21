2ee34f156f4b63ca01172833f7ce0022ff0d3e01
Tizen Ye <yzs981130@126.com>
2019-06-22 00:22:35 +0800

    add libcuda2 build

--- build.sh
+++ build.sh
@@ -1,1 +1,2 @@
 gcc -I /usr/local/cuda-10.0/include/ cuda-wrapper.c -fPIC -shared -ldl -lcuda -o ./release/libcuda.so
+gcc -I /usr/local/cuda-10.0/include/ cuda-wrapper2.c -fPIC -shared -ldl -lcuda -o ./release/libcuda2.so
