d9e8cba92ab2c8b054c3d87469ca84aa97809505
叶志晟 <yzs981130@126.com>
2019-06-24 19:38:31 +0800

    use quota instead of percent restriction

--- cuda-wrapper2.c
+++ cuda-wrapper2.c
@@ -4,42 +4,96 @@
 #include <cuda.h>
 #include <string.h>
 #include <pthread.h>
+#define SIZE 10000
+unsigned long long mod = 9973L;
 
 static const char LIB_STRING[] = "libcuda.so";
 
-bool open_flag = false;
+int open_flag = 0;
 void *handle = NULL;
-static size_t total_alloc = 0L;
-static size_t total_free = 0L;
-static size_t total_quota = 3821390336L;
-static pthread_mutex_t alloc_cnt_lock;
-static pthread_mutex_t free_cnt_lock;
+static size_t total_mem = 0L;
+static size_t total_quota = 4217928960L; //default set to 4GB
+static pthread_mutex_t mem_cnt_lock;
+char *error;
+
+struct HashArray
+{
+    unsigned long long key;
+    size_t value;
+    struct HashArray* next;
+}allocsize[10000];
 
 #define checkCudaErrors(err)  __checkCudaErrors (err, __FILE__, __LINE__)
-CUresult __checkCudaErrors( CUresult err, const char *file, const int line )
-{
+CUresult __checkCudaErrors( CUresult err, const char *file, const int line ) {
     if( CUDA_SUCCESS != err) {
         fprintf(stderr,
                 "CUDA Driver API error = %04d from file <%s>, line %i.\n",
                 err, file, line );
         //exit(-1);
     }
-	return err;
+    return err;
+}
+
+void addHash(unsigned long long key,size_t value) {
+    int temp=key%mod;
+    if(allocsize[temp].key==0) {
+        allocsize[temp].key=key;
+        allocsize[temp].value=value;
+	    printf("allocsize %lld %zu\n", key, value);
+    } 
+    else if(allocsize[temp].key==key) {
+        allocsize[temp].value=value;     
+    } 
+    else {
+        struct HashArray *p=&allocsize[temp]; 		
+        while(p->key!=key&&p->next!=NULL) {  
+            p=p->next;
+        }
+        if(p->key==key) {
+            p->value=value;
+        } 
+        else {
+            p->next=(struct HashArray*)malloc(sizeof(struct HashArray));
+            p=p->next;
+            p->key=key;
+            p->value=value;
+            p->next=NULL;
+        }
+    }   
+}
+size_t getHash(unsigned long long key) {
+    int temp=key%mod;
+    struct HashArray *p=&allocsize[temp];
+    if (p == NULL) {
+	    printf("miss\n");
+    	return 0;
+    }
+    printf("pkey: %lld\n", p->key);
+    while(p->key!=key&&p->next!=NULL) {
+	    p=p->next;
+    }		
+    if (p->key == key) {
+        printf("hit\n");
+        return p->value;
+    }
+    else {
+        printf("hash hit and miss\n");
+        return 0;
+    }
 }
 
 void init_func() {
-	if(open_flag == false && handle != NULL) {
-		char *error;
+    if(open_flag == 0 && handle == NULL) {
+        //char *error;
     	handle = dlopen (LIB_STRING, RTLD_LAZY);
     	if (!handle) {
-        	fprintf (stderr, "%s\n", dlerror());
-        	exit(1);
+       	    fprintf (stderr, "%s\n", dlerror());
+       	    exit(1);
     	}
-		open_flag = true;
+	    open_flag = 1;
     	dlerror();
-	}
-	pthread_mutex_init(&alloc_cnt_lock, NULL);
-	pthread_mutex_init(&free_cnt_lock, NULL);
+    }
+    pthread_mutex_init(&mem_cnt_lock, NULL);
 }
 
 void before_func() {
@@ -48,13 +102,14 @@ void before_func() {
 
 
 void post_func() {
-    //dlclose(handle);
+
 }
 
 
 CUresult cuInit(unsigned int Flags) {
-	init_func();
+    init_func();
     before_func();
+    printf("init!!!\n");
     CUresult (*fakecuInit)(unsigned int);
     fakecuInit = dlsym(handle, "cuInit");
 
@@ -62,88 +117,101 @@ CUresult cuInit(unsigned int Flags) {
         fprintf (stderr, "%s\n", error);
         exit(1);
     }
-	post_func();
-    return checkCudaErrors((*fakecuInit)(Flags));
+    post_func();
+    CUresult r;
+    r = checkCudaErrors((*fakecuInit)(Flags));
+    return r;
 }
 
 
 CUresult cuMemGetInfo_v2(size_t *free, size_t *total) {
     before_func();
-
     CUresult (*fakecuMemGetInfo_v2)(size_t *, size_t *);
     fakecuMemGetInfo_v2 = dlsym(handle, "cuMemGetInfo_v2");
-
     if ((error = dlerror()) != NULL)  {
         fprintf (stderr, "%s\n", error);
         exit(1);
     }
-	CUresult r;
+    CUresult r;
     r = checkCudaErrors((*fakecuMemGetInfo_v2)(free, total));
-    *free =  *free / 2;
-    *total = *total / 2;
-    printf("free : %zu, total : %zu\n", *free, *total);
-	post_func();
-
-	return r;
-
+    //*free =  *free / 2;
+    //*total = *total / 2;
+    printf("cumemgetinfo: free : %zu, total : %zu\n", *free, *total);
+    post_func();
+    return r;
 }
 
-bool check_alloc_valid(size_t bytesize) {
-	pthread_mutex_lock(&alloc_cnt_lock);	
-	pthread_mutex_lock(&free_cnt_lock);
-	if(total_alloc + bytesize - total_free > total_quota) {
-		fprintf (stderr, "alloc %zu failed, total_alloc %zu, total_free %zu, quota %zu\n", bytesize, total_alloc, total_free, total_quota);
-		pthread_mutex_unlock(&free_cnt_lock, NULL);
-		pthread_mutex_unlock(&alloc_cnt_lock, NULL);
-		return false;
-	}
-	pthread_mutex_unlock(&free_cnt_lock, NULL);
-	pthread_mutex_unlock(&alloc_cnt_lock, NULL);
-	return true;
+int check_alloc_valid(size_t bytesize) {
+    printf("lock mem in check_alloc_valid\n");
+    pthread_mutex_lock(&mem_cnt_lock);	
+    if(total_mem + bytesize > total_quota) {
+        fprintf (stderr, "alloc %zu failed, total_mem %zu, quota %zu\n", bytesize, total_mem,  total_quota);
+        printf("unlock mem in check_alloc_valid\n");
+        pthread_mutex_unlock(&mem_cnt_lock);
+        return 0;
+    }
+    printf("unlock mem in check\n");
+    pthread_mutex_unlock(&mem_cnt_lock);
+    return 1;
 }
 
 CUresult cuMemAlloc_v2(CUdeviceptr* dptr, size_t bytesize) {
     before_func();
-
     CUresult (*fakecuMemAlloc_v2)(CUdeviceptr* , size_t);
     fakecuMemAlloc_v2 = dlsym(handle, "cuMemAlloc_v2");
-
     if ((error = dlerror()) != NULL)  {
         fprintf (stderr, "%s\n", error);
         exit(1);
     }
-	post_func();
+    post_func();
 
-	if(check_alloc_valid(bytesize)) {
-		CUresult r = checkCudaErrors((*fakecuMemAlloc_v2)(dptr, bytesize));
-		if(CUDA_SUCCESS == r) {
-			pthread_mutex_lock(&alloc_cnt_lock);
-			total_alloc += bytesize;
-			pthread_mutex_unlock(&alloc_cnt_lock, NULL);
-		}
-		return r;
-	}
-	else {
-		return CUDA_ERROR_OUT_OF_MEMORY;
-	}
+    if(check_alloc_valid(bytesize)) {
+        printf("lock mem in cumemalloc\n");
+        pthread_mutex_lock(&mem_cnt_lock);
+        total_mem += bytesize;
+        //printf("%d\n", dptr);
+        //printf("bs: %zu\n", bytesize);
+        pthread_mutex_unlock(&mem_cnt_lock);
+        printf("unlock mem in cumemalloc\n");
+        CUresult r = checkCudaErrors((*fakecuMemAlloc_v2)(dptr, bytesize));
+        if(CUDA_SUCCESS != r) {
+            printf("lock if r != CUDA_SUCCESS\n");
+            pthread_mutex_lock(&mem_cnt_lock);
+            total_mem -= bytesize;                  
+            pthread_mutex_unlock(&mem_cnt_lock);
+            printf("unlock if r != CUDA_SUCCESS\n ");
+        }
+        else {			
+            addHash((unsigned long long)dptr,bytesize);
+            //TODO: assert
+            printf("cumemalloc: hash insert with bytesize %zu\n", bytesize);
+            printf("cumemalloc: hash insert with bytesize %zu\n", getHash((unsigned long long)dptr));
+        }
+        return r;
+    }
+    else {
+        return CUDA_ERROR_OUT_OF_MEMORY;
+    }
 }
 
 CUresult cuMemFree_v2(CUdeviceptr dptr) {
     before_func();
-
     CUresult (*fakecuMemFree_v2)(CUdeviceptr);
     fakecuMemFree_v2 = dlsym(handle, "cuMemFree_v2");
-
     if ((error = dlerror()) != NULL)  {
         fprintf (stderr, "%s\n", error);
         exit(1);
     }
-	post_func();
+    post_func();
     CUresult r = checkCudaErrors((*fakecuMemFree_v2)(dptr));
-	if(CUDA_SUCCESS == r) {
-		pthread_mutex_lock(&free_cnt_lock);
-		total_free = 0;
-		pthread_mutex_unlock(&free_cnt_lock, NULL);
-	}
-	return r;
+    if(CUDA_SUCCESS == r) {
+        printf("lock mem in cumemfree\n");
+        pthread_mutex_lock(&mem_cnt_lock);
+        size_t tbytesize=getHash(dptr);
+        printf("%lld %zu",dptr, tbytesize);
+        total_mem -= tbytesize;
+        printf("lock mem in cumemfree\n");
+        pthread_mutex_unlock(&mem_cnt_lock);
+    }
+    return r;
 }
