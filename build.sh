29d45fe32d953129558e910f62059679ff0d42d2
Tizen Ye <yzs981130@126.com>
2019-06-24 20:50:41 +0800

    fix include path in build.sh

--- build.sh
+++ build.sh
@@ -1,2 +1,2 @@
-gcc -I /usr/local/cuda-10.0/include/ cuda-wrapper.c -fPIC -shared -ldl -lcuda -o ./release/libcuda.so
-gcc -I /usr/local/cuda-10.0/include/ cuda-wrapper2.c -fPIC -shared -ldl -lcuda -o ./release/libcuda2.so
+gcc -I /usr/local/cuda/include/ cuda-wrapper.c -fPIC -shared -ldl -lcuda -o ./release/libcuda.so
+gcc -I /usr/local/cuda/include/ cuda-wrapper2.c -fPIC -shared -ldl -lcuda -o ./release/libcuda2.so
