222233df9602f65cb846524fa43725d79db0a811
Tizen Ye <yzs981130@126.com>
2019-06-25 14:12:09 +0800

    fix build.sh

--- build.sh
+++ build.sh
@@ -1,2 +1,3 @@
+mkdir -p release
 gcc -I /usr/local/cuda/include/ cuda-wrapper.c -fPIC -shared -ldl -lcuda -o ./release/libcuda.so
 gcc -I /usr/local/cuda/include/ cuda-wrapper2.c -fPIC -shared -ldl -lcuda -o ./release/libcuda2.so
